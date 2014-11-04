11be69752212b5788708f7b6222e56850457a6b4
Andrei Karas <akaras@inbox.ru>
2014-11-04 23:12:02 +0300

    Initial commit.

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone git@gitorious.org:evol/clientdata.git client-data
+git clone git@gitorious.org:evol/hercules.git server-code
+git clone git@gitorious.org:evol/serverdata.git server-data
+git clone git@gitorious.org:evol/evol-tools.git tools
