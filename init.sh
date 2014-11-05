0628d73427b1f3461ecde5639e7b5ce8839dcbfb
Andrei Karas <akaras@inbox.ru>
2014-11-05 17:53:52 +0300

    Add ability to init.sh clone only task related repos.
    Build in tasks:
        all
        client
        server
        music
        default
    
    Example: ./init.sh client

--- init.sh
+++ init.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-git clone git@gitorious.org:evol/clientdata.git client-data
-git clone git@gitorious.org:evol/hercules.git server-code
-git clone git@gitorious.org:evol/serverdata.git server-data
-git clone git@gitorious.org:evol/evol-tools.git tools
-git clone git@gitorious.org:evol/evol-hercules.git server-code/src/evol
+CMD="$1"
+if [[ -z "${CMD}" ]]; then
+    export CMD="default"
+fi
 
-if [[ "$1" == "all" ]]; then
+if [[ "${CMD}" == "all" || "${CMD}" == "default" ]]; then
+    git clone git@gitorious.org:evol/clientdata.git client-data
+    git clone git@gitorious.org:evol/hercules.git server-code
+    git clone git@gitorious.org:evol/serverdata.git server-data
+    git clone git@gitorious.org:evol/evol-tools.git tools
+    git clone git@gitorious.org:evol/evol-hercules.git server-code/src/evol
+elif [[ "${CMD}" == "server" ]]; then
+    git clone git@gitorious.org:evol/hercules.git server-code
+    git clone git@gitorious.org:evol/serverdata.git server-data
+    git clone git@gitorious.org:evol/evol-hercules.git server-code/src/evol
+elif [[ "${CMD}" == "client" ]]; then
+    git clone git@gitorious.org:evol/clientdata.git client-data
+    git clone git@gitorious.org:evol/evol-tools.git tools
+elif [[ "${CMD}" == "music" ]]; then
+    git clone git@gitorious.org:evol/evol-music.git music
+fi
+
+if [[ "${CMD}" == "all" ]]; then
     git clone git@gitorious.org:evol/evol-music.git music
 fi
