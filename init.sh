a62058341bcab5993ae5286013f329b9dee163c2
Andrei Karas <akaras@inbox.ru>
2014-11-12 17:01:57 +0300

    Create top dir link to evol hercules plugin.

--- init.sh
+++ init.sh
@@ -11,10 +11,12 @@ if [[ "${CMD}" == "all" || "${CMD}" == "default" ]]; then
     git clone git@gitorious.org:evol/serverdata.git server-data
     git clone git@gitorious.org:evol/evol-tools.git tools
     git clone git@gitorious.org:evol/evol-hercules.git server-code/src/evol
+    ln -s server-code/src/evol server-plugin
 elif [[ "${CMD}" == "server" ]]; then
     git clone git@gitorious.org:evol/hercules.git server-code
     git clone git@gitorious.org:evol/serverdata.git server-data
     git clone git@gitorious.org:evol/evol-hercules.git server-code/src/evol
+    ln -s server-code/src/evol server-plugin
 elif [[ "${CMD}" == "client" ]]; then
     git clone git@gitorious.org:evol/clientdata.git client-data
     git clone git@gitorious.org:evol/evol-tools.git tools
