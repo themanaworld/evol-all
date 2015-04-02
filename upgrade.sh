73cdb8a9cdb0f2d0762c2855106ab3f348bbe4fe
Andrei Karas <akaras@inbox.ru>
2015-04-02 14:41:44 +0300

    add update.sh for upgrade to new repository urls.
    How to run: ./upgrade.sh gitlab

---
+++ upgrade.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function status {
+    if [ ! -d "$1" ]; then
+        return
+    fi
+    DIR=$(pwd)
+    cd "$1"
+    echo "$2:"
+    URL=$(git config remote.origin.url|sed "s/gitorious.org/gitlab.com/")
+    git config remote.origin.url ${URL}
+    echo "set origin to ${URL}"
+    cd "$DIR"
+}
+
+CMD="$1"
+
+if [[ "${CMD}" != "gitlab" ]]; then
+    exit 1
+fi
+
+status . evol-all
+status client-data client-data
+status server-code server-code
+status server-data server-data
+status server-local server-local
+status tools tools
+status server-code/src/evol evol-hercules
+status music music
+status manaplus manaplus
