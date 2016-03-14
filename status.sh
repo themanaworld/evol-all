7f8234729c5540382291b9a121ecd2f4248f42d5
Andrei Karas <akaras@inbox.ru>
2016-03-14 19:29:07 +0300

    Check db version 4.

--- status.sh
+++ status.sh
@@ -19,7 +19,7 @@ function dbinfo {
     export VER=$(cat tools/localserver/versions/sqlver 2>/dev/null)
 
     if [[ -n "${VER}" ]]; then
-        if [ "${VER}" -lt "3" ]; then
+        if [ "${VER}" -lt "4" ]; then
             reportdb
         fi
         return
