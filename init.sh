75265aea655b9fd377cc29d1351768562da77439
Andrei Karas <akaras@inbox.ru>
2016-06-20 23:49:44 +0300

    Move group name into variable.

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export LANG=C
+export GROUP=evol
 
 CMD="$1"
 PROTO="git@gitlab.com:"
@@ -14,44 +15,44 @@ if [[ -n "${STR}" ]]; then
 fi
 
 if [[ "${CMD}" == "all" || "${CMD}" == "default" ]]; then
-    git clone ${PROTO}evol/clientdata.git client-data
-    git clone ${PROTO}evol/hercules.git server-code
-    git clone ${PROTO}evol/serverdata.git server-data
-    git clone ${PROTO}evol/evol-tools.git tools
-    git clone ${PROTO}evol/evol-hercules.git server-code/src/evol
-    git clone ${PROTO}evol/evol-local.git server-local
-    git clone ${PROTO}evol/evol-docs.git docs
+    git clone ${PROTO}${GROUP}/clientdata.git client-data
+    git clone ${PROTO}${GROUP}/hercules.git server-code
+    git clone ${PROTO}${GROUP}/serverdata.git server-data
+    git clone ${PROTO}${GROUP}/evol-tools.git tools
+    git clone ${PROTO}${GROUP}/evol-hercules.git server-code/src/evol
+    git clone ${PROTO}${GROUP}/evol-local.git server-local
+    git clone ${PROTO}${GROUP}/evol-docs.git docs
     ln -s server-code/src/evol server-plugin
 elif [[ "${CMD}" == "server" ]]; then
-    git clone ${PROTO}evol/hercules.git server-code
-    git clone ${PROTO}evol/serverdata.git server-data
-    git clone ${PROTO}evol/evol-hercules.git server-code/src/evol
-    git clone ${PROTO}evol/evol-tools.git tools
+    git clone ${PROTO}${GROUP}/hercules.git server-code
+    git clone ${PROTO}${GROUP}/serverdata.git server-data
+    git clone ${PROTO}${GROUP}/evol-hercules.git server-code/src/evol
+    git clone ${PROTO}${GROUP}/evol-tools.git tools
     ln -s server-code/src/evol server-plugin
 elif [[ "${CMD}" == "client" ]]; then
-    git clone ${PROTO}evol/clientdata.git client-data
-    git clone ${PROTO}evol/evol-tools.git tools
+    git clone ${PROTO}${GROUP}/clientdata.git client-data
+    git clone ${PROTO}${GROUP}/evol-tools.git tools
 elif [[ "${CMD}" == "music" ]]; then
-    git clone ${PROTO}evol/evol-music.git music
+    git clone ${PROTO}${GROUP}/evol-music.git music
 elif [[ "${CMD}" == "local" ]]; then
-    git clone ${PROTO}evol/evol-local.git server-local
-    git clone ${PROTO}evol/hercules.git server-code
-    git clone ${PROTO}evol/serverdata.git server-data
-    git clone ${PROTO}evol/evol-hercules.git server-code/src/evol
-    git clone ${PROTO}evol/evol-tools.git tools
+    git clone ${PROTO}${GROUP}/evol-local.git server-local
+    git clone ${PROTO}${GROUP}/hercules.git server-code
+    git clone ${PROTO}${GROUP}/serverdata.git server-data
+    git clone ${PROTO}${GROUP}/evol-hercules.git server-code/src/evol
+    git clone ${PROTO}${GROUP}/evol-tools.git tools
     ln -s server-code/src/evol server-plugin
 elif [[ "${CMD}" == "tools" ]]; then
-    git clone ${PROTO}evol/evol-tools.git tools
+    git clone ${PROTO}${GROUP}/evol-tools.git tools
 elif [[ "${CMD}" == "docs" ]]; then
-    git clone ${PROTO}evol/evol-docs.git docs
+    git clone ${PROTO}${GROUP}/evol-docs.git docs
 elif [[ "${CMD}" == "manaplus" ]]; then
     git clone ${PROTO}manaplus/manaplus.git manaplus
 elif [[ "${CMD}" == "media" ]]; then
-    git clone ${PROTO}evol/evol-media.git media
+    git clone ${PROTO}${GROUP}/evol-media.git media
 fi
 
 if [[ "${CMD}" == "all" ]]; then
-    git clone ${PROTO}evol/evol-music.git music
+    git clone ${PROTO}${GROUP}/evol-music.git music
     git clone ${PROTO}manaplus/manaplus.git manaplus
-    git clone ${PROTO}evol/evol-media.git media
+    git clone ${PROTO}${GROUP}/evol-media.git media
 fi
