c69af7824a0268a55bad8b92ff5c565306e93f4f
Andrei Karas <akaras@inbox.ru>
2017-12-26 01:12:01 +0300

    Update sql db check to version 10.

--- status.sh
+++ status.sh
@@ -19,7 +19,7 @@ function dbinfo {
     export VER=$(cat tools/localserver/versions/sqlver 2>/dev/null)
 
     if [[ -n "${VER}" ]]; then
-        if [ "${VER}" -lt "9" ]; then
+        if [ "${VER}" -lt "10" ]; then
             reportdb
         fi
         return
