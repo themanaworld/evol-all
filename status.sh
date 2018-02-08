cb2950b5170fd1077a3e9454d4c87bbc29a66760
Andrei Karas <akaras@inbox.ru>
2018-02-08 20:26:34 +0300

    Update sql db check to version 11.

--- status.sh
+++ status.sh
@@ -19,7 +19,7 @@ function dbinfo {
     export VER=$(cat tools/localserver/versions/sqlver 2>/dev/null)
 
     if [[ -n "${VER}" ]]; then
-        if [ "${VER}" -lt "10" ]; then
+        if [ "${VER}" -lt "11" ]; then
             reportdb
         fi
         return
