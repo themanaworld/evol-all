4830e1f5b926b4808e9f70fab0899dc0d456f498
Andrei Karas <akaras@inbox.ru>
2018-07-13 22:21:57 +0300

    Update sql db check to version 14.

--- status.sh
+++ status.sh
@@ -19,7 +19,7 @@ function dbinfo {
     export VER=$(cat tools/localserver/versions/sqlver 2>/dev/null)
 
     if [[ -n "${VER}" ]]; then
-        if [ "${VER}" -lt "13" ]; then
+        if [ "${VER}" -lt "14" ]; then
             reportdb
         fi
         return
