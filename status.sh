d0d5844b8cc951002ba53acfd06180038bbbd7e5
Andrei Karas <akaras@inbox.ru>
2019-05-13 23:22:35 +0300

    Update sql db check to version 18

--- status.sh
+++ status.sh
@@ -19,7 +19,7 @@ function dbinfo {
     export VER=$(cat tools/localserver/versions/sqlver 2>/dev/null)
 
     if [[ -n "${VER}" ]]; then
-        if [ "${VER}" -lt "17" ]; then
+        if [ "${VER}" -lt "18" ]; then
             reportdb
         fi
         return
